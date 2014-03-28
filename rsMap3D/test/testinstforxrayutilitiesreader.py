f0adb6c0a5e48cf75d0d7080f0b072ece79b8d88
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-03-28 15:35:46 +0000

    SSG_000116-78 Add in test case for monitorName not in the file.  This may be an optional parameter(only used for scaling).  Return None if monitor not included.  Check getSample/DetectorCircles.  Return an element list if they are defined, raise IO Exception otherwise.

--- rsMap3D/test/testinstforxrayutilitiesreader.py
+++ rsMap3D/test/testinstforxrayutilitiesreader.py
@@ -11,8 +11,12 @@ class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.config = InstForXrayutilitiesReader('../resources/33BM-instForXrayutilities.xml')
-        #self.config2 = 
+        self.config = InstForXrayutilitiesReader( \
+                     '../resources/33BM-instForXrayutilities.xml')
+        self.config2 = InstForXrayutilitiesReader( \
+                      '../resources/33BM-instForXrayutilities-noMonitor.xml')
+        self.config3 = InstForXrayutilitiesReader( \
+                      '../resources/33BM-instForXrayutilities-noCircles.xml')
 
     def tearDown(self):
         pass
@@ -22,6 +26,24 @@ class Test(unittest.TestCase):
         monitorName = self.config.getMonitorName()
         self.assertEquals(monitorName, "I0", "getMonitorName")
 
+    def testGetMonitorNameNoMonitor(self):
+        monitorName = self.config2.getMonitorName()
+        self.assertEquals(monitorName, None, "getMonitorNameNoMonitor")
+
+    def testGetDetectorCircles(self):
+        circles = self.config.getDetectorCircles()
+        self.assertEqual(len(circles), 1, "getDetectorCircles")
+        
+    def testGetDetectorCirclesNoCircle(self):
+        self.assertRaises(IOError, self.config3.getDetectorCircles)
+        
+    def testGetSampleCircles(self):
+        circles = self.config.getSampleCircles()
+        self.assertEqual(len(circles), 3, "getSampleCircles")
+        
+    def testGetSampleCirclesNoCircle(self):
+        self.assertRaises(IOError, self.config3.getSampleCircles)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testGetMonitorName']
     unittest.main()
