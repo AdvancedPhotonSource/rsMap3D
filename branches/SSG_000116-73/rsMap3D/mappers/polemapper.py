792db6ec23b0535e1c47124a29e28539946a0dcc
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-03-31 15:31:59 +0000

    SSG_000116-73 Create branch for task work

---
+++ branches/SSG_000116-73/rsMap3D/mappers/polemapper.py
@@ -0,0 +1,39 @@
+'''
+ Copyright (c) 2012, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+import numpy as np
+
+import xrayutilities as xu
+from rsMap3D.mappers.abstractmapper import AbstractGridMapper
+
+class PoleFigureMapper(AbstractGridMapper):
+    '''
+    '''
+
+    def processMap(self, **kwargs):
+        """
+        read ad frames and grid them in reciprocal space
+        angular coordinates are taken from the spec file
+    
+        **kwargs are passed to the rawmap function
+        """
+    
+        gridder = xu.Gridder3D(self.nx, self.ny ,self.nz)
+        gridder.KeepData(True)
+        rangeBounds = self.dataSource.getRangeBounds()
+        gridder.dataRange((rangeBounds[0], rangeBounds[1]), 
+                          (rangeBounds[2], rangeBounds[3]), 
+                          (rangeBounds[4], rangeBounds[5]), 
+                          True)
+        
+        imageToBeUsed = self.dataSource.getImageToBeUsed()
+        for scan in self.dataSource.getAvailableScans():
+            #print '---' + str(scan) + str(imageToBeUsed[scan])
+            if True in imageToBeUsed[scan]:
+                qx, qy, qz, intensity = self.rawmap((scan, ), **kwargs)
+                
+                # convert data to rectangular grid in reciprocal space
+                gridder(qx, qy, qz ,intensity)
+    
+        return gridder.xaxis,gridder.yaxis,gridder.zaxis,gridder.data,gridder
