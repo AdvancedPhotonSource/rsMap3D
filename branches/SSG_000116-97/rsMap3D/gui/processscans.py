adda2359483361043672f48d579e03fd92b41cd5
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-04-14 15:37:17 +0000

    SGG_000116-97 Create Branch for bad pixel file.

---
+++ branches/SSG_000116-97/rsMap3D/gui/processscans.py
@@ -0,0 +1,145 @@
+'''
+ Copyright (c) 2012, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+import os
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QGridLayout
+from PyQt4.QtGui import QLabel
+from PyQt4.QtGui import QPushButton
+from PyQt4.QtGui import QComboBox
+from PyQt4.QtGui import QLineEdit
+from PyQt4.QtGui import QFileDialog
+from PyQt4.QtGui import QMessageBox
+
+from rsMap3D.mappers.gridmapper import QGridMapper
+from rsMap3D.mappers.polemapper import PoleFigureMapper
+
+class ProcessScans(QDialog):
+    '''
+    This class presents a form to select to start analysis.  This display
+    allows switching between Grid map and pole figure.
+    '''
+    POLE_MAP_STR = "Pole Map"
+    GRID_MAP_STR = "Grid Map"
+    
+    def __init__(self, parent=None):
+        '''
+        Constructor - Layout widgets on the page & link up actions.
+        '''
+        super(ProcessScans, self).__init__(parent)
+        layout = QGridLayout()
+        label = QLabel("Output Type")        
+#        layout.addWidget(label, 0, 0)
+        self.outTypeChooser = QComboBox()
+        self.outTypeChooser.addItem(self.GRID_MAP_STR)
+        self.outTypeChooser.addItem(self.POLE_MAP_STR)
+#        layout.addWidget(self.outTypeChooser, 0,1)
+        
+        label = QLabel("Grid Dimensions")
+        layout.addWidget(label, 0,0)
+        
+        label = QLabel("X")
+        layout.addWidget(label, 1,0)
+        self.xDimTxt = QLineEdit()
+        self.xDimTxt.setText("200")
+        layout.addWidget(self.xDimTxt, 1,1)
+        
+        label = QLabel("Y")
+        layout.addWidget(label, 2,0)
+        self.yDimTxt = QLineEdit()
+        self.yDimTxt.setText("200")
+        layout.addWidget(self.yDimTxt, 2,1)
+        
+        label = QLabel("Z")
+        layout.addWidget(label, 3,0)
+        self.zDimTxt = QLineEdit()
+        self.zDimTxt.setText("200")
+        layout.addWidget(self.zDimTxt, 3,1)
+        
+        label = QLabel("Output File")
+        layout.addWidget(label, 4,0)
+        self.outFileTxt = QLineEdit()
+        layout.addWidget(self.outFileTxt, 4,1)
+        self.outputFileButton = QPushButton("Browse")
+        layout.addWidget(self.outputFileButton, 4, 2)
+        
+        
+        runButton = QPushButton("Run")
+        layout.addWidget(runButton, 5, 3)
+        self.setLayout(layout)                    
+        self.connect(runButton, SIGNAL("clicked()"), self.process)
+        self.connect(self.outputFileButton, SIGNAL("clicked()"), 
+                     self.browseForOutputFile)
+        self.connect(self.outputFileButton, SIGNAL("editFinished()"), 
+                     self.editFinishedOutputFile)
+        
+        
+        
+    def process(self):
+        '''
+        Emit a signal to trigger the start of procesing.
+        '''
+        self.emit(SIGNAL("process"))
+        
+    def runMapper(self, dataSource, transform):
+        '''
+        Run the selected mapper
+        '''
+        self.dataSource = dataSource
+        print "Selected " + str(self.outTypeChooser.currentText())
+        nx = int(self.xDimTxt.text())
+        ny = int(self.yDimTxt.text())
+        nz = int(self.zDimTxt.text())
+        outputFileName = str(self.outFileTxt.text())
+        if (self.outTypeChooser.currentText() == self.GRID_MAP_STR):
+            gridMapper = QGridMapper(dataSource, \
+                                     outputFileName, \
+                                     nx=nx, ny=ny, nz=nz,
+                                     transform = transform)
+            gridMapper.doMap()
+        else:
+            poleMapper = PoleFigureMapper(dataSource, \
+                                          outputFileName, \
+                                          nx=nx, ny=ny, nz=nz, \
+                                          transform = transform)
+            poleMapper.doMap()
+            
+    
+    def browseForOutputFile(self):
+        if self.outFileTxt.text() == "":
+            fileName = QFileDialog.getSaveFileName(None, \
+                                               "Save File", \
+                                               filter="*.vti")
+        else:
+            fileDirectory = os.path.dirname(str(self.OutputFileTxt.text()))
+            fileName = QFileDialog.getOpenFileName(None, 
+                                               "Save File", 
+                                               filter="*.vti", \
+                                               directory = fileDirectory)
+        if fileName != "":
+            if os.path.exists(os.path.dirname(str(fileName))):
+                self.outFileTxt.setText(fileName)
+                self.outFileTxt.emit(SIGNAL("editingFinished()"))
+            else:
+                message = QMessageBox()
+                message.warning(self, \
+                             "Warning"\
+                             "The specified directory does not exist")
+                self.outFileTxt.setText(fileName)
+                self.outFileTxt.emit(SIGNAL("editingFinished()"))
+                
+    def editFinishedOutputFile(self):
+        fileName = str(self.outFileTxt.text())
+        if fileName != "":
+            if os.path.exists(os.path.dirname(fileName)):
+                self.outFileTxt.setText(fileName)
+                self.outFileTxt.emit(SIGNAL("editingFinished()"))
+            else:
+                message = QMessageBox()
+                message.warning(self, \
+                             "Warning"\
+                             "The specified directory does not exist")
+        
---
+++ branches/SSG_000116-97/rsMap3D/rsmEdit.py
@@ -0,0 +1,162 @@
+'''
+ Copyright (c) 2012, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import QWidget
+from PyQt4.QtGui import QGridLayout
+from PyQt4.QtGui import QTabWidget
+from PyQt4.QtGui import QMessageBox
+from PyQt4.QtGui import QApplication
+ 
+from rsMap3D.datasource.Sector33SpecDataSource import Sector33SpecDataSource
+from rsMap3D.datasource.Sector33SpecDataSource import LoadCanceledException
+from rsMap3D.gui.scanform import ScanForm
+from rsMap3D.gui.fileform import FileForm
+from rsMap3D.gui.datarange import DataRange
+from rsMap3D.gui.processscans import ProcessScans
+from rsMap3D.transforms.unitytransform3d import UnityTransform3D
+from rsMap3D.transforms.polemaptransform3d import PoleMapTransform3D
+
+import sys
+import traceback
+
+class MainDialog(QWidget):
+    '''
+    '''
+    def __init__(self,parent=None):
+        '''
+        '''
+        super(MainDialog, self).__init__(parent)
+        layout = QGridLayout()
+        self.tabs = QTabWidget()
+        self.fileForm =FileForm()
+        self.scanForm = ScanForm()
+        self.dataRange = DataRange()
+        self.processScans = ProcessScans()
+        self.fileTabIndex = self.tabs.addTab(self.fileForm, "File")
+        self.dataTabIndex = self.tabs.addTab(self.dataRange, "Data Range")
+        self.scanTabIndex = self.tabs.addTab(self.scanForm, "Scans")
+        self.processTabIndex = self.tabs.addTab(self.processScans, 
+                                                "Process Data")
+        self.tabs.setTabEnabled(self.dataTabIndex, False)
+        self.tabs.setTabEnabled(self.scanTabIndex, False)
+        self.tabs.setTabEnabled(self.processTabIndex, False)
+        self.tabs.show()
+        layout.addWidget(self.tabs)
+        self.setLayout(layout)
+
+        self.connect(self.fileForm, SIGNAL("loadFile"), self.spawnLoadThread)
+        self.connect(self.fileForm, SIGNAL("cancelLoadFile"), 
+                     self.cancelLoadThread)
+        self.connect(self.scanForm, SIGNAL("doneLoading"), self.setupRanges)
+        self.connect(self.dataRange, SIGNAL("rangeChanged"), self.setScanRanges)
+        self.connect(self.tabs, SIGNAL("currentChanged(int)"), self.tabChanged)
+        self.connect(self.processScans, SIGNAL("process"), self.runMapper)
+        self.connect(self, SIGNAL("fileError"), self.showFileError)
+        
+    def cancelLoadThread(self):
+        self.dataSource.signalCancelLoadSource()
+        
+        
+    def loadScanFile(self):
+        '''
+        '''
+        self.tabs.setTabEnabled(self.dataTabIndex, False)
+        self.tabs.setTabEnabled(self.scanTabIndex, False)
+        self.tabs.setTabEnabled(self.processTabIndex, False)
+        if self.fileForm.getOutputType() == self.fileForm.SIMPLE_GRID_MAP_STR:
+            self.transform = UnityTransform3D()
+        elif self.fileForm.getOutputType() == self.fileForm.POLE_MAP_STR:
+            self.transform = PoleMapTransform3D()
+        else:
+            self.transform = None
+            
+             
+        try:
+            self.dataSource = \
+                Sector33SpecDataSource(str(self.fileForm.getProjectDir()), \
+                                       str(self.fileForm.getProjectName()), \
+                                       str(self.fileForm.getProjectExtension()), \
+                                       str(self.fileForm.getInstConfigName()), \
+                                       str(self.fileForm.getDetConfigName()), \
+                                       transform = self.transform, \
+                                       scanList = self.fileForm.getScanList(), \
+                                       roi = self.fileForm.getDetectorROI(), \
+                                       pixelsToAverage = \
+                                          self.fileForm.getPixelsToAverage(), \
+                                      )
+            self.dataSource.loadSource(mapHKL = self.fileForm.getMapAsHKL())
+        except LoadCanceledException as e:
+            print "LoadCanceled"
+            self.tabs.setTabEnabled(self.dataTabIndex, False)
+            self.tabs.setTabEnabled(self.scanTabIndex, False)
+            self.tabs.setTabEnabled(self.processTabIndex, False)
+            self.fileForm.setLoadOK()
+            return
+        except Exception as e:
+            self.emit(SIGNAL("fileError"), str(e))
+            print traceback.format_exc()
+            return
+        
+        self.scanForm.loadScanFile(self.dataSource)        
+        self.fileForm.setLoadOK()
+        
+    def spawnLoadThread(self):
+        self.fileForm.setCancelOK()
+        self.loadThread = LoadScanThread(self, parent=None)
+        self.loadThread.start()
+        
+    def setupRanges(self):
+        '''
+        '''
+        overallXmin, overallXmax, overallYmin, overallYmax, \
+               overallZmin, overallZmax = self.dataSource.getOverallRanges()
+        self.dataRange.setRanges(overallXmin, \
+                                 overallXmax, \
+                                 overallYmin, \
+                                 overallYmax, \
+                                 overallZmin, \
+                                 overallZmax)
+        self.setScanRanges()
+        self.tabs.setTabEnabled(self.dataTabIndex, True)
+        self.tabs.setTabEnabled(self.scanTabIndex, True)
+        self.tabs.setTabEnabled(self.processTabIndex, True)
+    
+    def setScanRanges(self):
+        '''
+        '''
+        ranges = self.dataRange.getRanges()
+        self.dataSource.setRangeBounds(ranges)
+        self.scanForm.renderOverallQs()
+
+    def showFileError(self, error):
+        message = QMessageBox()
+        message.warning(self, \
+                            "Load Scanfile Warning", \
+                             str(error))
+              
+    def tabChanged(self, index):
+        '''
+        '''
+        if str(self.tabs.tabText(index)) == "Data Range":
+            self.scanForm.renderOverallQs()
+                                        
+    def runMapper(self):
+        self.processScans.runMapper(self.dataSource, self.transform)
+        
+class LoadScanThread(QThread):
+    def __init__(self, controller, **kwargs):
+        super(LoadScanThread, self).__init__( **kwargs)
+        self.controller = controller
+        
+    def run(self):
+        self.controller.loadScanFile()
+        
+        
+app = QApplication(sys.argv)
+mainForm = MainDialog()
+mainForm.show()
+app.exec_()
+
