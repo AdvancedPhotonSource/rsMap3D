e69493c95c173c6311335aead8d4118dc15fd2c6
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-05-07 18:51:34 +0000

    SSG_000116-159 Change all signal names which come from qt into text fields in qtsignalstrings.py

--- branches/SSG_000116-136/rsMap3D/gui/qtsignalstrings.py
+++ branches/SSG_000116-136/rsMap3D/gui/qtsignalstrings.py
@@ -5,6 +5,7 @@
 #Signal Strings
 BUTTON_CLICKED_SIGNAL = "buttonClicked(QAbstractButton *)"
 CLICKED_SIGNAL = "clicked()"
+CURRENT_TAB_CHANGED = "currentChanged(int)"
 CURRENT_INDEX_CHANGED_SIGNAL = "currentIndexChanged(QString)"
 EDIT_FINISHED_SIGNAL = "editingFinished()"
 LIST_ITEM_CLICKED_SIGNAL = "itemClicked(QListWidgetItem *)"
