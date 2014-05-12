2cf604875b9bd0152cce83665b57bc4d67bf41bc
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-05-12 20:43:21 +0000

    SSG_000116-174 Add documention to all methods in the class.  Start using Sphinx formatting and add information on parameters and returns.

--- branches/SSG_000116-133/rsMap3D/transforms/abstracttransform3d.py
+++ branches/SSG_000116-133/rsMap3D/transforms/abstracttransform3d.py
@@ -11,4 +11,10 @@ class AbstractTransform3D():
     
     @abc.abstractmethod
     def do3DTransform(self, axis1Data, axis2Data, axis3Data):
+        '''Perform the actual transform of the data.  Note that in this parent 
+        class this method is abstract
+        :param axis1Data:  data from axis 1 to be transformed
+        :param axis2Data:  data from axis 2 to be transformed
+        :param axis3Data:  data from axis 3 to be transformed
+        '''
         print ("Using AbstractTransform3D.doTransform3D")
