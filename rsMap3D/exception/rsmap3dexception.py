7d8959b18034322a3da101c8076528c7bdce6214
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-05-13 15:58:38 +0000

    SSG_000116-133 merge branch to trunk

--- rsMap3D/exception/rsmap3dexception.py
+++ rsMap3D/exception/rsmap3dexception.py
@@ -7,15 +7,12 @@ class RSMap3DException(Exception):
     General base class for all exceptions in rsMap3D package.
     '''
     def __init__(self, message):
+        '''
+        Constructor
+        :param message: Message string to pass along when the exception is 
+        raised.
+        '''
         super(RSMap3DException, self).__init__(message)
-
-class InstConfigException(RSMap3DException):
-    '''
-    Exception class to be raised if there is a problem with an instrument config
-    file
-    '''
-    def __init__(self, message):
-        super(InstConfigException, self).__init__(message)
 
 class DetectorConfigException(RSMap3DException):
     '''
@@ -25,12 +22,30 @@ class DetectorConfigException(RSMap3DException):
     def __init__(self, message):
         super(DetectorConfigException, self).__init__(message)
 
+class InstConfigException(RSMap3DException):
+    '''
+    Exception class to be raised if there is a problem with an instrument config
+    file
+    '''
+    def __init__(self, message):
+        '''
+        Constructor
+        :param message: Message string to pass along when the exception is 
+        raised.
+        '''
+        super(InstConfigException, self).__init__(message)
+
 class Transform3DException(RSMap3DException):
     '''
     Exception class to be raised if there is a problem with a detector config
     file
     '''
     def __init__(self, message):
+        '''
+        Constructor
+        :param message: Message string to pass along when the exception is 
+        raised.
+        '''
         super(Transform3DException, self).__init__(message)
 
 class ScanDataMissingException(RSMap3DException):
@@ -39,5 +54,10 @@ class ScanDataMissingException(RSMap3DException):
     shows that the data is missing
     '''
     def __init__(self, message):
+        '''
+        Constructor
+        :param message: Message string to pass along when the exception is 
+        raised.
+        '''
         super(ScanDataMissingException, self).__init__(message)
 
--- rsMap3D/transforms/polemaptransform3d.py
+++ rsMap3D/transforms/polemaptransform3d.py
@@ -14,10 +14,23 @@ class PoleMapTransform3D(AbstractTransform3D):
     Transform axes for stereographic projection.
     '''
     def __init__(self, projectionDirection=POS_Z_DIRECTION):
+        '''
+        Contructor
+        :param projectionDirection: direction of axis of projection.   Note that
+        currently only [0,0,1] (+Z) and [0,0,-1] (-Z) are allowed
+        '''
         self.projectionDirection = projectionDirection
     
     def do3DTransform(self, axis1Data, axis2Data, axis3Data):
-        
+        '''Perform the actual transform of the data.  Note that in this parent 
+        class this method is abstract
+        :param axis1Data:  data from axis 1 to be transformed
+        :param axis2Data:  data from axis 2 to be transformed
+        :param axis3Data:  data from axis 3 to be transformed
+        :return: Transformed axes (X & Y projected to plane at the center of
+        of the sphere and Z transformed to |q|
+        '''
+
         if self.projectionDirection == POS_Z_DIRECTION:
             axis3DataOut = np.sqrt(axis1Data**2 + axis2Data**2 + axis3Data**2)
             axis1DataOut = axis1Data/(axis3DataOut + axis3Data)
