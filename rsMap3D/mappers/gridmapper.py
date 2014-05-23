6842fb39e8c1215e69f912977bd5caa0bb844f06
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2014-05-23 20:26:03 +0000

    SSG_000116-181 & SSG_000116-182 Add in application config file with maxImageMemory.  Use memory limit to chunk data processing within a scan.

--- rsMap3D/mappers/gridmapper.py
+++ rsMap3D/mappers/gridmapper.py
@@ -5,12 +5,14 @@
 
 import xrayutilities as xu
 from rsMap3D.mappers.abstractmapper import AbstractGridMapper
+import numpy as np
+from rsMap3D.config.rsmap3dconfig import RSMap3DConfig
+
 
 class QGridMapper(AbstractGridMapper):
     '''
     This map provides an x, y, z grid of the data.
     '''
-
     def processMap(self, **kwargs):
         """
         read ad frames and grid them in reciprocal space
@@ -18,7 +20,8 @@ class QGridMapper(AbstractGridMapper):
     
         **kwargs are passed to the rawmap function
         """
-    
+        rsMap3DConfig = RSMap3DConfig()
+        maxImageMem = rsMap3DConfig.getMaxImageMemory()
         gridder = xu.Gridder3D(self.nx, self.ny, self.nz)
         gridder.KeepData(True)
         rangeBounds = self.dataSource.getRangeBounds()
@@ -28,16 +31,38 @@ class QGridMapper(AbstractGridMapper):
                           True)
         
         imageToBeUsed = self.dataSource.getImageToBeUsed()
-        progress = 1
+        progress = 0
         for scan in self.dataSource.getAvailableScans():
 
             if True in imageToBeUsed[scan]:
-                qx, qy, qz, intensity = self.rawmap((scan,), **kwargs)
-                
-                # convert data to rectangular grid in reciprocal space
-                gridder(qx, qy, qz, intensity)
-            if self.progressUpdater <> None:
-                self.progressUpdater(progress)
-            progress += 1
+                imageSize = self.dataSource.getDetectorDimensions()[0] * \
+                            self.dataSource.getDetectorDimensions()[1]
+                numImages = len(imageToBeUsed[scan])
+                if imageSize*4*numImages <= maxImageMem:
+                    qx, qy, qz, intensity = self.rawmap((scan,), **kwargs)
+                    
+                    # convert data to rectangular grid in reciprocal space
+                    gridder(qx, qy, qz, intensity)
+                    progress += 100
+                    if self.progressUpdater <> None:
+                        self.progressUpdater(progress)
+                else:
+                    nPasses = imageSize*4*numImages/ maxImageMem + 1
+                    
+                    for thisPass in range(nPasses):
+                        imageToBeUsedInPass = np.array(imageToBeUsed[scan])
+                        imageToBeUsedInPass[:thisPass*numImages/nPasses] = False
+                        imageToBeUsedInPass[(thisPass+1)*numImages/nPasses:] = False
+                        
+                        kwargs['mask'] = imageToBeUsedInPass
+                        qx, qy, qz, intensity = \
+                            self.rawmap((scan,), **kwargs)
+                        
+                        # convert data to rectangular grid in reciprocal space
+                        gridder(qx, qy, qz, intensity)
+                    
+                        progress += 1.0/nPasses* 100.0
+                        if self.progressUpdater <> None:
+                            self.progressUpdater(progress)
             
         return gridder.xaxis,gridder.yaxis,gridder.zaxis,gridder.data,gridder
