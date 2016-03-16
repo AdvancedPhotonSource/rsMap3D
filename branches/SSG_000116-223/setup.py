34df1dc05ee387a14377f719de0fdc52dec19e31
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-03-16 19:11:53 +0000

    First try at a setup file.

---
+++ branches/SSG_000116-223/setup.py
@@ -0,0 +1,27 @@
+'''
+ Copyright (c) 2014, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+from setuptools import setup
+
+setup(name='rsMap3D',
+      version='1.0.0',
+      description='Python Program to map xray diffraction data into ' + \
+                    'reciprocal space map',
+      author = 'John Hammonds, Christian Schleputz',
+      author_email = 'JPHammonds@anl.gov',
+      url = 'https://confluence.aps.anl.gov/display/RSM/SSG_000116+Reciprocal+Space+Mapping',
+      packages = ['rsMap3D',
+                  'rsMap3D.config',
+                   'rsMap3D.datasource',
+                   'rsMap3D.datasource.DetectorGeometry',
+                   'rsMap3D.exception',
+                   'rsMap3D.gui',
+                   'rsMap3D.gui.input',
+                   'rsMap3D.transforms',
+                   'rsMap3D.utils',
+                   'rsMap3D.mappers'] ,
+      install_requires = ['spec2nexus',
+                 'pillow',
+                 ],
+      )
