2af988886a5c60b9d6c0a4491a8494400bdd0b31
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-04-25 18:20:27 +0000

    Replace loops over pixels with array multiplication in numpy.  This changes so that passing an array for qs instead of individual pixels.

--- rsMap3D/datasource/sector34nexusescansource.py
+++ rsMap3D/datasource/sector34nexusescansource.py
@@ -125,7 +125,7 @@ class Sector34NexusEscanSource(AbstractDataSource):
             values.append([energy,]) 
         return values
 
-    def loadSource(self):
+    def loadDetectorConfig(self):
         try:
             self.detConfig = DetectorGeometryForEScan(self.detConfigFile)
             self.detector = self.detConfig.getDetectorById(self.detectorId)
@@ -168,7 +168,9 @@ class Sector34NexusEscanSource(AbstractDataSource):
         except Exception as ex:
             print ("---Unhandled Exception in loading detector config")
             raise ex
-
+        
+    def loadSource(self):
+        self.loadDetectorConfig()
         
         if self.files == None:
             print("Getting file list")
@@ -219,19 +221,16 @@ class Sector34NexusEscanSource(AbstractDataSource):
                 print("Starting to get Energy independant Information")
                 xIndexArray = range(self.detectorROI[0], self.detectorROI[1] +1)
                 yIndexArray = range(self.detectorROI[2], self.detectorROI[3] +1)
-                arraySize = [len(xIndexArray), len(yIndexArray)]
-              
-                self.qpx =np.zeros(arraySize)
-                self.qpy =np.zeros(arraySize)
-                self.qpz =np.zeros(arraySize)
-                for row in yIndexArray:
-                    for column in xIndexArray:
-                        startX = self.detectorROI[0]
-                        startY = self.detectorROI[2]
-                        qForPixel = self.pixel2q(column, row, None)
-                        self.qpx[row-startX, column-startY] = qForPixel[0]
-                        self.qpy[row-startX, column-startY] = qForPixel[1]
-                        self.qpz[row-startX, column-startY] = qForPixel[2]
+
+                indexMesh = np.meshgrid(xIndexArray, yIndexArray)
+                qpxyz = self.pixel2q_2(indexMesh, None)
+                self.qpx, self.qpy, self.qpz = qpxyz[:,:,0], qpxyz[:,:,1], qpxyz[:,:,2]
+                print "self.qpx"
+                print self.qpx
+                print "self.qpy"
+                print self.qpy
+                print "self.qpz"
+                print self.qpz                      
                 print("Ending Energy independant Information")
 
         #reset progress bar for second pass
@@ -252,10 +251,36 @@ class Sector34NexusEscanSource(AbstractDataSource):
         xp += self.detectorTranslation[0]
         yp += self.detectorTranslation[1]
         zp = self.detectorTranslation[2]
+        if(pixelX == 1 and pixelY == 1):
+            print "xp,yp,zp"
+            print xp,yp,zp
         xyz = [(self.rho[0][0]*xp + self.rho[0][1]*yp + self.rho[0][2]*zp), \
                (self.rho[1][0]*xp + self.rho[1][1]*yp + self.rho[1][2]*zp), \
                (self.rho[2][0]*xp + self.rho[2][1]*yp + self.rho[2][2]*zp)]
         return xyz
+
+    def pixel2XYZ_2(self, indexMesh):
+            xp = np.subtract(indexMesh[0], 0.5*self.detectorDimensions[0])
+            xp = np.subtract(xp, 1.0)
+            xp = np.multiply(xp, self.detectorPixelWidth[0])
+            xp = np.add(xp, self.detectorTranslation[0])
+
+            yp = np.subtract(indexMesh[1], 0.5*self.detectorDimensions[1])
+            yp = np.subtract(yp, 1.0)
+            yp = np.multiply(yp, self.detectorPixelWidth[1])
+            yp = np.add(yp, self.detectorTranslation[1])
+            
+            zp = np.multiply(np.ones((self.detectorDimensions[0], self.detectorDimensions[1])), 
+                             self.detectorTranslation[2])
+            print "xp[0][0], yp[0][0], zp[0][0]"
+            print xp[0][0], yp[0][0], zp[0][0]
+            xpp = np.inner(self.rho[0,:], np.stack((xp, yp, zp), axis=-1))
+            ypp = np.inner(self.rho[1,:], np.stack((xp, yp, zp), axis=-1))
+            zpp = np.inner(self.rho[2,:], np.stack((xp, yp, zp), axis=-1))
+            xyz = np.stack((xpp, ypp, zpp), axis=-1)
+            
+            print "shape x,y,z" + str(xyz.shape)
+            return xyz
     
     def pixel2q(self, pixelX, pixelY, qhat, depth=0):
         ki = [0, 0, 1]
@@ -269,6 +294,26 @@ class Sector34NexusEscanSource(AbstractDataSource):
             kout /= koutnorm 
         
         qhat = kout-ki
+        return qhat
+    
+    def pixel2q_2(self, indexMesh, qhat, depth=0):
+        ki = [0, 0, 1]
+        
+        kout = self.pixel2XYZ_2(indexMesh)
+        
+        if depth != 0:
+            kout = np.subtract(kout, depth*ki)
+        koutnorm = np.linalg.norm(kout, axis=2)
+        print "koutnorm"
+        print koutnorm
+        print koutnorm.shape
+        print kout.shape
+        if np.any(koutnorm):
+            for index in range(np.ma.size(kout, 2)):
+                print "index " + str(index)
+                kout[:,:,index] = np.divide(kout[:,:,index], koutnorm) 
+        
+        qhat = np.subtract(kout,ki)
         return qhat
     
     def qsForDetector(self, startFile=0, endFile=0):     
@@ -292,19 +337,32 @@ class Sector34NexusEscanSource(AbstractDataSource):
         sizeXArray = len(xIndexArray)
         sizeYArray = len(yIndexArray)
         arrSize = [numFiles, sizeXArray, sizeYArray]
-        qx = np.zeros(arrSize)
-        qy = np.zeros(arrSize)
-        qz = np.zeros(arrSize)
+#         qx = np.zeros(arrSize)
+#         qy = np.zeros(arrSize)
+#         qz = np.zeros(arrSize)
+#         qx = []
+#         qy = []
+#         qz = []
+        twoPiOverLambda = []
         arrSize = [len(yIndexArray), \
                        len(xIndexArray)]
         for afile in processFiles:
             if self.progressUpdater <> None:
                 self.progressUpdater(self.progress, self.progressMax)
-            self.progress += self.progressInc
-            twoPiOverLamda = 2*np.pi * self.incidentEnergy[afile] / 1.23985
-            qx[afile-processFiles[0],:,:] = self.qpx * twoPiOverLamda 
-            qy[afile-processFiles[0],:,:] = self.qpy * twoPiOverLamda 
-            qz[afile-processFiles[0],:,:] = self.qpz * twoPiOverLamda 
+            self.progress += self.progressInc 
+            twoPiOverLambda.append(2*np.pi * self.incidentEnergy[afile] / 1.23985)
+            
+#             qx[afile-processFiles[0],:,:] = np.multiply(self.qpx, twoPiOverLamda) 
+#             qy[afile-processFiles[0],:,:] = np.multiply(self.qpy, twoPiOverLamda) 
+#             qz[afile-processFiles[0],:,:] = np.multiply(self.qpz, twoPiOverLamda) 
+#             qx.append(np.multiply(self.qpx, twoPiOverLamda)) 
+#             qy.append(np.multiply(self.qpy, twoPiOverLamda)) 
+#             qz.append(np.multiply(self.qpz, twoPiOverLamda)) 
+        qx = np.multiply.outer(twoPiOverLambda, self.qpx)
+        print "qx.shape"
+        print qx.shape
+        qy = np.multiply.outer(twoPiOverLambda, self.qpy)
+        qz = np.multiply.outer(twoPiOverLambda, self.qpz)
         return qx, qy, qz
 
     def rawmap(self,scans, angdelta=[0,0,0,0,0],
