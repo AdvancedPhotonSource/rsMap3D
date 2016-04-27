22d804cdcb5b665d7f02758559009cdf9657b9c6
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-04-27 15:57:06 +0000

    Update version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='rsMap3D',
-      version='1.0.2',
+      version='1.0.3,
       description='Python Program to map xray diffraction data into ' + \
                     'reciprocal space map',
       author = 'John Hammonds, Christian Schleputz',
