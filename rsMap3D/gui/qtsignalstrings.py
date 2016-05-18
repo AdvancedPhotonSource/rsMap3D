76bb3cf587493f9e85601a0e152693a0dab76b79
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-05-18 20:58:55 +0000

    Add string for state change of a checkbox

--- rsMap3D/gui/qtsignalstrings.py
+++ rsMap3D/gui/qtsignalstrings.py
@@ -10,4 +10,6 @@ CURRENT_INDEX_CHANGED_SIGNAL = "currentIndexChanged(QString)"
 EDIT_FINISHED_SIGNAL = "editingFinished()"
 LIST_ITEM_CLICKED_SIGNAL = "itemClicked(QListWidgetItem *)"
 TABLE_ITEM_CHANGED_SIGNAL = "itemChanged(QTableWidgetItem *)"
+STATE_CHANGED_SIGNAL = "stateChanged(int)"
 TEXT_CHANGED_SIGNAL = "textChanged(QString)"
+
