52ce8d6532dd3da3a9aa7d7c42b4b967037f650d
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-06-20 15:47:18 +0000

    Change information for version 1.0.10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='rsMap3D',
-      version='1.0.9',
+      version='1.0.10',
       description='Python Program to map xray diffraction data into ' + \
                     'reciprocal space map',
       author = 'John Hammonds, Christian Schleputz',
