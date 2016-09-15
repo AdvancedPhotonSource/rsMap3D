0f4461a780c6a893266f9640bbec75fdebc7bf0d
hammonds <hammonds@e8ece9b1-36bc-4f98-af6a-27220428c58f>
2016-09-15 17:09:38 +0000

    Start abstracting data output to allow for more than just output of grid to vti file.

---
+++ rsMap3D/mappers/output/abstactgridoutput.py
@@ -0,0 +1,46 @@
+'''
+ Copyright (c) 2016, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+import abc
+from abc import abstractmethod
+import inspect
+
+class AbstractGridOutput(object):
+    __metaclass__ = abc.ABCMeta
+    '''
+    This class is an abstract class to handle the output(writting) format 
+    of RSM data:
+    '''
+    
+    def __init__(self):
+        self.qx = None
+        self.qy = None
+        self.qz = None
+        self.gridData = None
+    
+    def setData(self,qx, qy, qz, gridData ):
+        self.qx = qx
+        self.qy = qy
+        self.qz = qz
+        self.gridData = gridData
+        self.fileInfo = []
+        
+    @abstractmethod
+    def setFileInfo(self, fileInfo):
+        """Set information necessary for creating filenames for
+        output files that will be created
+        :param fileInfo A tuple describing how filenames will be
+        generated
+        """
+        return
+        
+    @abstractmethod
+    def write(self):
+        """ Write the output data.  It is necessary to set the data 
+        and set the output file info before riting the files"""
+        return
+        
+        
+    def whatFunction(self):
+        return inspect.stack()[1][3]
---
+++ rsMap3D/mappers/output/vtigridoutput.py
@@ -0,0 +1,82 @@
+'''
+ Copyright (c) 2016, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+from rsMap3D.mappers.output.abstactgridoutput import AbstractGridOutput
+import numpy as np
+from vtk.util import numpy_support
+import vtk
+VTI_OUTFILE_MERGE_STR = "%s_S%d.vti"
+
+class VTIGridOutput(AbstractGridOutput):
+    
+    def setFileInfo(self, fileInfo):
+        if ((fileInfo == None) or (len(fileInfo) == 0)):
+            raise ValueError(self.whatFunction() +
+                            "passed no filename information " +
+                            "requires a tuple with six members:\n"
+                            "1. project filename so that a filename can be " +
+                            "constructed if filename is blank\n 2. User " +
+                            "filename, and number of pixels for output in " + 
+                            "x/y/z directions and output file name")
+        elif (len( fileInfo) != 6):
+            raise ValueError(self.whatFunction() +
+                            "passed no filename information " +
+                            "requires a tuple with six members:\n"
+                            "1. project filename so that a filename can be " +
+                            "constructed if filename is blank\n 2. User " +
+                            "filename, and number of pixels for output in " + 
+                            "x/y/z directions and outputFileName")
+        self.fileInfo.append(fileInfo[0])
+        self.fileInfo.append(fileInfo[1])
+        self.nx = fileInfo[2]
+        self.ny = fileInfo[3]
+        self.nz = fileInfo[4]
+        self.outputFileName = fileInfo[5]
+        
+        
+        
+    def write(self):
+        qx0 = self.qx.min()
+        dqx = (self.qx.max()-qx0)/self.nx
+        qy0 = self.qy.min()
+        dqy = (self.qx.max()-qy0)/self.ny
+        qz0 = self.qz.min()
+        dqz = (self.qz.max()-qz0)/self.nz
+        
+        da = np.transpose(self.gridData).reshape(self.gridData.size)
+        data_array = numpy_support.numpy_to_vtk(da)
+        
+        image_data = vtk.vtkImageData()
+        image_data.SetOrigin(qx0,qy0, qz0)
+        image_data.SetSpacing(dqx, dqy, dqz)
+        print self.nx, self.ny,self.nx
+        image_data.SetExtent(0, self.nx-1,\
+                             0, self.ny-1,\
+                             0, self.nz-1)
+        if vtk.VTK_MAJOR_VERSION <= 5:
+            image_data.SetNumberOfScalarComponents(1)
+            image_data.SetScalarTypeToDouble()
+        else:
+            image_data.AllocateScalars(vtk.VTK_DOUBLE, 1)
+               
+        pd = image_data.GetPointData()
+        
+        pd.SetScalars(data_array)
+        
+        # Export data to file
+        writer= vtk.vtkXMLImageDataWriter()
+        if self.outputFileName == "":
+            writer.SetFileName(VTI_OUTFILE_MERGE_STR % 
+                               (self.fileInfo[0], \
+                                self.fileInfo[1]))
+        else:
+            writer.SetFileName(self.outputFileName)
+            
+        if vtk.VTK_MAJOR_VERSION <= 5:
+            writer.SetInput(image_data)
+        else:
+            writer.SetInputData(image_data)
+            
+        writer.Write()
+
