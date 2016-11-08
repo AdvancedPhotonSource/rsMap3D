52a5f096b4dd79f941bf6ab20a487545990da3a7
John Hammonds <JPHammonds@anl.gov>
2016-11-08 09:56:36 -0600

    Changes to commit version 17

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='rsMap3D',
-      version='1.0.16',
+      version='1.0.17',
       description='Python Program to map xray diffraction data into ' + \
                     'reciprocal space map',
       author = 'John Hammonds, Christian Schleputz',
