8b33027514ba6a599aeb96a8c2ded22635ceac28
John Hammonds <JPHammonds@anl.gov>
2017-03-10 00:12:47 -0600

    Add in file to configure the logger with a dictionary in case fileConfig does not find the configuration file in the user's home directory.

---
+++ rsMap3D/config/rsmap3dlogging.py
@@ -0,0 +1,24 @@
+'''
+ Copyright (c) 2017, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+LOGGER_NAME = "rsMap3D"
+LOGGER_FORMAT = '%(asctime)-15s - %(name)s - %(funcName)s- %(levelname)s - %(message)s'
+LOGGER_DEFAULT = {
+    'version' : 1,
+    'handlers' : {'consoleHandler' : {'class' : 'logging.StreamHandler',
+                               'level' : 'INFO',
+                               'formatter' : 'consoleFormat',
+                               'stream' : 'ext://sys.stdout'}
+                  },
+    'formatters' : {'consoleFormat' : {'format' : '%(asctime)-15s - %(name)s - %(funcName)s- %(levelname)s - %(message)s'}
+                    },
+    'loggers' : {'root' :{'level' : 'INFO',
+                        'handlers' : ['consoleHandler',],
+                      },
+               'rsMap3D' : {'level' : 'INFO',
+                            'handlers' : ['consoleHandler',],
+                            'qualname' : 'rsMap3D'
+                            }
+               },
+   }
