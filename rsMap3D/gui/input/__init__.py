03453229682264582a52a3acaea197c5a681d5e7
John Hammonds <JPHammonds@anl.gov>
2017-04-03 14:22:34 -0500

    Setup to import actual scan classes so that these can be found for more automated configuration.

---
+++ rsMap3D/gui/input/__init__.py
@@ -0,0 +1,5 @@
+from .s1highenergydiffractionform import S1HighEnergyDiffractionForm
+from .s33specscanfileform import S33SpecScanFileForm
+from .s34hdfescanfileform import S34HDFEScanFileForm
+from .xpcsspecscanfileform import XPCSSpecScanFileForm
+from .fileinputcontroller import FileInputController
