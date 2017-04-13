125e36145fcfe79b7348c0b81c93fab620fa945a
John Hammonds <JPHammonds@anl.gov>
2017-04-13 09:55:29 -0500

    Changes to adapt to new configparser

--- rsMap3D/config/rsmap3dconfig.py
+++ rsMap3D/config/rsmap3dconfig.py
@@ -8,35 +8,51 @@ from os.path import exists
 import xml.etree.ElementTree as ET
 from rsMap3D.exception.rsmap3dexception import RSMap3DException
 from xml.dom import minidom
+import logging
+logger = logging.getLogger(__name__)
 
 NAMESPACE = '{https://subversion.xray.aps.anl.gov/RSM/rsMap3DConfig}'
 MAX_IMAGE_MEMORY = NAMESPACE + "maxImageMemory"
-
+CONFIG_FILENAME_DEFAULT = ".rsMap3D"
+MAX_IMAGE_MEMORY_BYTES = "100000000"
 
 class RSMap3DConfig():
     """
     Class to read and store configuration information for the rsMap3D 
     application
     """
-    def __init__(self, fileName=""):
+    def __init__(self, fileName="", exeptionOnNoFile=False):
         if fileName == "":
             userDir = expanduser("~")
-            self.configFile = join(userDir,".rsMap3D")
+            self.configFile = join(userDir, CONFIG_FILENAME_DEFAULT)
+            logger.debug("No filename given so using default name " + \
+                         str(self.configFile))
         else:
             self.configFile = fileName
+            logger.debug("Filename passed as argument using that " + \
+                         str(self.configFile))
         try:
             if not exists(self.configFile):
-                self.createConfigFile()
+                #normally this just creates a new config file.  Add allowing
+                #exception when a new config bases on ConfigParser was adopted
+                #This allows using an reading old config file, if it exists,
+                #to set the maxMemory specified by the user.
+                if not exeptionOnNoFile:
+                    self.createConfigFile()
+                else:
+                    raise RSMap3DConfigNotFound("File " + self.configFile +
+                                                " was not found")
             tree = ET.parse(self.configFile)
         except IOError as ex:
             raise (RSMap3DException("Bad config file for rsMap3D " + 
-                                    str(self.configFile)))
+                                    str(self.configFile) + "\n" +
+                                    str(ex)))
         self.root = tree.getroot()
         
     def createConfigFile(self):
         configElement = ET.Element('rsMap3DConfig', xmlns = NAMESPACE[1:-1])
         maxIMem = ET.SubElement(configElement, 'maxImageMemory')
-        maxIMem.text ="100000000"
+        maxIMem.text = MAX_IMAGE_MEMORY_BYTES
         #ET.dump(configElement)
         tree = ET.ElementTree(configElement)
         tree.write(self.configFile,  encoding = "utf-8", xml_declaration=True, method ="xml")
@@ -57,6 +73,14 @@ class RSMap3DConfig():
             raise RSMap3DException("Config file " + self.configFile + \
                                " maxImageMemory does not set a number correctly")
 
+class RSMap3DConfigNotFound(RSMap3DException):
+    '''
+    Exception specific to not finding default RSMap3DConfig file
+    '''
+    def __init__(self, message):
+        super(RSMap3DConfigNotFound, self).__init__(message)
+        
+    
 def main():
     config = RSMap3DConfig()
     config.createConfigFile()
