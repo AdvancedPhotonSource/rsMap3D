895650076a1ede76761fa1bfd49b32c56b03e3cc
John Hammonds <JPHammonds@anl.gov>
2017-06-08 10:29:59 -0500

    Remove entries in this file.  They were added during trying to figure out dynamic loading and do not seem to be needed,

--- rsMap3D/gui/input/__init__.py
+++ rsMap3D/gui/input/__init__.py
@@ -1,5 +1,5 @@
-from .s1highenergydiffractionform import S1HighEnergyDiffractionForm
-from .s33specscanfileform import S33SpecScanFileForm
-from .s34hdfescanfileform import S34HDFEScanFileForm
-from .xpcsspecscanfileform import XPCSSpecScanFileForm
-from .fileinputcontroller import FileInputController
+# from .s1highenergydiffractionform import S1HighEnergyDiffractionForm
+# from .s33specscanfileform import S33SpecScanFileForm
+# from .s34hdfescanfileform import S34HDFEScanFileForm
+# from .xpcsspecscanfileform import XPCSSpecScanFileForm
+# from .fileinputcontroller import FileInputController
